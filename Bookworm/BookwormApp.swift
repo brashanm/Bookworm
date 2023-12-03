1e2e6d9af4fba093d88f6262ea891f6021dccbdd
Brashan <brashanm@gmail.com>
2023-12-03 00:27:35 -0500

    Initial Commit

---
+++ Bookworm/BookwormApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookwormApp.swift
+//  Bookworm
+//
+//  Created by Brashan Mohanakumar on 2023-12-03.
+//
+
+import SwiftUI
+
+@main
+struct BookwormApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
